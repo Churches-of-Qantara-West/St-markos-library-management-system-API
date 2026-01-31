67900307e93dea582531dc416bc02f01fa98974c
GergesHany <gergeshany179@gmail.com>
2026-01-31 16:18:54 +0200

    Added verification.model, verification.repository and verification.schemas and remove verification.dto

---
+++ src/shared/models/verification.model.ts
@@ -0,0 +1,6 @@
+export class VerificationModel {
+  email: string;
+  verificationCode: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserRepository } from './repositories/user.repository';
+import { VerificationRepository } from './repositories/verification.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schemas';
+import { Verification, VerificationSchema } from './schemas/verification.schemas';
 
 @Module({
   imports: [
-    MongooseModule.forFeature(
-        [
-            { 
-                name: User.name, 
-                schema: UserSchema 
-            }
-        ]
-    )],
-  providers: [UserRepository],
-  exports: [UserRepository],
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Verification.name, schema: VerificationSchema },
+    ]),
+  ],
+  providers: [UserRepository, VerificationRepository],
+  exports: [UserRepository, VerificationRepository],
 })
 export class SharedModule {}
