7d34c59ac6f43793d0888f2abbe52283fbf206bb
GergesHany <gergeshany179@gmail.com>
2026-01-31 14:39:26 +0200

    2.Moved Auth into app/Auth

--- src/Auth/dto/verification.dto.ts
+++
@@ -1,8 +0,0 @@
-import { RegisterDto } from "./register.dto";
-import { IsNotEmpty, Length } from "class-validator";
-
-export class VerificationDto extends RegisterDto {
-    @IsNotEmpty()
-    @Length(6, 6)
-    verificationCode: string;
-}
